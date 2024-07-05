fca90b1564852cab7251a443dc48c4286c29a204
stevesato423 <stevesato423@gmail.com>
2024-07-05 12:13:21 -0400

    cloudwatch event rules for different logics added

--- infrastructure/cloudwatch.tf
+++ infrastructure/cloudwatch.tf
@@ -3,25 +3,64 @@ resource "aws_cloudwatch_log_group" "this" {
   retention_in_days = 365
 }
 
-
-resource "aws_cloudwatch_event_rule" "scheduled" {
-  name        = "${local.function_name}-lambda-trigger"
-  description = "Event that triggers Lambda function"
-  # We can reduce the interval according to the execution duration of the Lambda function
-  schedule_expression = "rate(2 minutes)"
-
-  state = "ENABLED"
+resource "aws_cloudwatch_event_rule" "fetch_raw_transfer_event" {
+  name                = "${local.function_name}-fetchRawTransferEvent-trigger"
+  description         = "Event that triggers Lambda function to fetch raw transfer events"
+  schedule_expression = "rate(1 minute)"
+  state               = "ENABLED"
 }
 
-resource "aws_cloudwatch_event_target" "scheduled_lambda" {
-  rule = aws_cloudwatch_event_rule.scheduled.name
+resource "aws_cloudwatch_event_target" "fetch_raw_transfer_event_lambda" {
+  rule = aws_cloudwatch_event_rule.fetch_raw_transfer_event.name
   arn  = aws_lambda_function.this.arn
+
+  input = jsonencode({
+    operation = "fetchRawTransferEvent"
+  })
+}
+
+resource "aws_cloudwatch_event_rule" "fetch_tvl" {
+  name                = "${local.function_name}-fetchTVL-trigger"
+  description         = "Event that triggers Lambda function to fetch TVL"
+  schedule_expression = "rate(2 minutes)"
+  state               = "ENABLED"
+}
+
+resource "aws_cloudwatch_event_target" "fetch_tvl_lambda" {
+  rule = aws_cloudwatch_event_rule.fetch_tvl.name
+  arn  = aws_lambda_function.this.arn
+
+  input = jsonencode({
+    operation = "fetchTVL"
+  })
+}
+
+resource "aws_cloudwatch_event_rule" "fetch_revenue" {
+  name                = "${local.function_name}-fetchRevenue-trigger"
+  description         = "Event that triggers Lambda function to fetch revenue"
+  schedule_expression = "rate(3 minutes)"
+  state               = "ENABLED"
+}
+
+resource "aws_cloudwatch_event_target" "fetch_revenue_lambda" {
+  rule = aws_cloudwatch_event_rule.fetch_revenue.name
+  arn  = aws_lambda_function.this.arn
+
+  input = jsonencode({
+    operation = "fetchRevenue"
+  })
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id  = "AllowExecutionFromCloudWatch"
+  for_each = {
+    fetch_raw_transfer_event = aws_cloudwatch_event_rule.fetch_raw_transfer_event.arn
+    fetch_tvl                = aws_cloudwatch_event_rule.fetch_tvl.arn
+    fetch_revenue            = aws_cloudwatch_event_rule.fetch_revenue.arn
+  }
+
+  statement_id  = "AllowExecutionFromCloudWatch-${each.key}"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.this.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.scheduled.arn
+  source_arn    = each.value
 }
--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,26 @@ import { ironTokenAddresses } from './shared/ironTokenAddresses';
 import { usdcContractAbi } from './abi/usdc';
 import { ironUsdcContractAbi } from './abi/iron-usdc';
 
+/**
+ * Provide an event that contains the following keys:
+ * - operation: one of 'fetchRawTransferEvent', 'fetchTVL', or 'fetchRevenue'
+ */
+
 export const handler: Handler = async (event, context: Context): Promise<void> => {
 
-    await FetchRawTransferEvent(contractConstants.rpcUrl, ironTokenAddresses.ironUSDC, ironUsdcContractAbi, contractConstants.explorerAddress, contractConstants.MAX_RANGE);
-    // TVL of Ironclad USDC
-    await GetBalance(contractConstants.rpcUrl, contractConstants.usdcAddress, usdcContractAbi, ironTokenAddresses.ironUSDC);
-    // Revenue
-    await GetBalance(contractConstants.rpcUrl, ironTokenAddresses.ironUSDC, ironUsdcContractAbi, contractConstants.treasuryAddress);
+    const operation = event.operation;
+
+    switch (operation) {
+        case 'fetchRawTransferEvent':
+            await FetchRawTransferEvent(contractConstants.rpcUrl, ironTokenAddresses.ironUSDC, ironUsdcContractAbi, contractConstants.explorerAddress, contractConstants.MAX_RANGE);
+            break;
+        case 'fetchTVL':
+            await GetBalance(contractConstants.rpcUrl, contractConstants.usdcAddress, usdcContractAbi, ironTokenAddresses.ironUSDC);
+            break;
+        case 'fetchRevenue':
+            await GetBalance(contractConstants.rpcUrl, ironTokenAddresses.ironUSDC, ironUsdcContractAbi, contractConstants.treasuryAddress);
+            break;
+        default:
+            console.log(`Unknown operation: ${operation}`);
+    }
 };
