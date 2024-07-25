a702421eb06acef02ac4c4ba984e88abd3c911de
stevesato423 <stevesato423@gmail.com>
2024-07-25 00:33:59 -0400

    Wrap five charts into for-loop in dashboard.tf

--- infrastructure/dashboard.tf
+++ infrastructure/dashboard.tf
@@ -3,91 +3,32 @@ data "aws_region" "current" {}
 locals {
   namespace  = "Contract-Metrics"
   aws_region = data.aws_region.current.name
+  symbol     = "IronUSDC"
+  metrics    = ["Revenue", "TVL", "Deposit", "Debt", "TMS"]
 }
 
 resource "aws_cloudwatch_dashboard" "main" {
-  dashboard_name = "${local.function_name}-dashboard"
+  dashboard_name = "${local.function_name}-${local.symbol}-dashboard"
 
   dashboard_body = jsonencode({
-    "widgets" : [
-      {
-        "height" : 6,
-        "width" : 6,
-        "y" : 0,
-        "x" : 0,
-        "type" : "metric",
-        "properties" : {
-          "view" : "timeSeries",
-          "stacked" : false,
-          "metrics" : [
-            [local.namespace, "IronUSDC TVL", "TVL", "USDC"]
-          ],
-          "region" : local.aws_region
+    "widgets" : flatten([
+      for idx, metric in local.metrics : [
+        {
+          "height" : 6,
+          "width" : 6,
+          "y" : floor(idx / 4) * 6,
+          "x" : (idx % 4) * 6,
+          "type" : "metric",
+          "properties" : {
+            "view" : "timeSeries",
+            "stacked" : false,
+            "metrics" : [
+              [local.namespace, "${local.symbol} ${metric}", metric, local.symbol]
+            ],
+            "region" : local.aws_region
+          }
         }
-      },
-      {
-        "height" : 6,
-        "width" : 6,
-        "y" : 0,
-        "x" : 6,
-        "type" : "metric",
-        "properties" : {
-          "view" : "timeSeries",
-          "stacked" : false,
-          "metrics" : [
-            [local.namespace, "IronUSDC Revenue", "Revenue", "USDC"]
-          ],
-
-          "region" : local.aws_region,
-          # "period": 300,
-          # "stat": "Average"
-        }
-      },
-      {
-        "height" : 6,
-        "width" : 6,
-        "y" : 0,
-        "x" : 12,
-        "type" : "metric",
-        "properties" : {
-          "view" : "timeSeries",
-          "stacked" : false,
-          "metrics" : [
-            [local.namespace, "IronUSDC Deposit", "Deposit", "USDC"]
-          ],
-          "region" : local.aws_region
-        }
-      },
-      {
-        "height" : 6,
-        "width" : 6,
-        "y" : 0,
-        "x" : 18,
-        "type" : "metric",
-        "properties" : {
-          "view" : "timeSeries",
-          "stacked" : false,
-          "metrics" : [
-            [local.namespace, "IronUSDC Debt", "Debt", "USDC"]
-          ],
-          "region" : local.aws_region
-        }
-      },
-      {
-        "height" : 6,
-        "width" : 6,
-        "y" : 6,
-        "x" : 0,
-        "type" : "metric",
-        "properties" : {
-          "view" : "timeSeries",
-          "stacked" : false,
-          "metrics" : [
-            [local.namespace, "IronUSDC TMS", "TMS", "USDC"]
-          ],
-          "region" : local.aws_region
-        }
-      }
-    ]
+      ]
+    ])
   })
 }
