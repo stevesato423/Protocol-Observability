32daeb350a2f37e0a77e775ee799add7cf7a8be7
stevesato423 <stevesato423@gmail.com>
2024-07-30 22:51:02 -0400

    Add total metrics into dashboard

--- infrastructure/dashboard.tf
+++ infrastructure/dashboard.tf
@@ -3,7 +3,7 @@ data "aws_region" "current" {}
 locals {
   namespace  = "Contract-Metrics"
   aws_region = data.aws_region.current.name
-  symbols    = ["IronUSDC", "IronUSDT", "IronWETH", "IronezETH", "IronweETH", "IronwrsETH", "IronMBTC", "IronweETHmode", "IronMODE"]
+  symbols    = ["IronUSDC", "IronUSDT", "IronWETH", "IronezETH", "IronweETH", "IronwrsETH", "IronMBTC", "IronweETHmode", "IronMODE", "Total"]
   metrics    = ["Revenue", "TVL", "Deposit", "Debt", "TMS"]
 }
 
@@ -17,9 +17,9 @@ resource "aws_cloudwatch_dashboard" "main" {
       for idx, metric in local.metrics : [
         {
           "height" : 6,
-          "width" : 6,
-          "y" : floor(idx / 4) * 6,
-          "x" : (idx % 4) * 6,
+          "width" : 4,
+          "y" : 0,
+          "x" : idx * 4,
           "type" : "metric",
           "properties" : {
             "view" : "timeSeries",
@@ -29,7 +29,22 @@ resource "aws_cloudwatch_dashboard" "main" {
             ],
             "region" : local.aws_region
           }
-        }
+        }, 
+        {
+          "height" : 6,
+          "width" : 4,
+          "y" : 6,
+          "x" : idx * 4,
+          "type" : "metric",
+          "properties" : {
+            "view" : "singleValue",
+            "stacked" : false,
+            "metrics" : [
+              [local.namespace, "${each.value} ${metric}", metric, each.value]
+            ],
+            "region" : local.aws_region
+          }
+        }, 
       ]
     ])
   })
